06730a4d58d6b6d2351b411a0a0dbb0453fd0fb3
Laura Beatriz <49999046+lauradevcode@users.noreply.github.com>
2023-01-19 19:19:17 -0300

    :feet: "Adicionando js"

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// criando um objeto
+const celular = {
+  cor: "preto",
+  ligar: function () {
+    const mensagem = "Ligando"
+    alert(mensagem)
+  },
+}
+
+// usando um objeto
+// alert(celular.cor) // preto
+celular.ligar() // executa função
