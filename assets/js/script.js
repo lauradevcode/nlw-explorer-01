2307c6f7e246b6829f407db05f8dd45e78f8744d
Laura Beatriz <49999046+lauradevcode@users.noreply.github.com>
2023-01-20 01:26:20 -0300

    :construction: Calendário dinamico

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,27 @@
-// criando um objeto
-const celular = {
-  cor: "preto",
-  ligar: function () {
-    const mensagem = "Ligando"
-    alert(mensagem)
-  },
+const form = document.querySelector("#form-habits")
+const nlwSetup = new NLWSetup(form)
+
+const data = {
+  run: [
+    "01-01",
+    "01-02",
+    "01-06",
+    "01-07",
+    "01-08",
+    "01-09",
+    "01-10",
+    "01-11",
+    "01-12",
+    "01-13",
+    "01-14",
+    "01-15",
+  ],
+  water: ["01-04", "01-05"],
+  food: ["01-01", "01-03"],
+  journal: ["01-02"],
+  takePills: ["01-03"],
 }
 
-// usando um objeto
-// alert(celular.cor) // preto
-celular.ligar() // executa função
+nlwSetup.setData(data)
+nlwSetup.load()
+console.log(data)
